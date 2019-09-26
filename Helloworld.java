77dbede426344eb85c345a2200c3d0dde127f616
nadnadia25 <55502535+nadnadia25@users.noreply.github.com>
2019-09-26 08:41:17 +0800

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+public class Helloworld{
+
+    public static void main(String[] args){
+        int A = 3*6/2;
+        int B = 3*4/2;
+        int C = 4*3;
+        System.out.println("hello World Malaysia STIA1113");
+    }
+    System.out.printIn("A= " +0.5*3*6);
+    System.out.printIn("B= " +0.5*3*4);
+    System.out.printIn("C= " +4*3);
+    System.out.printIn("A+B+C= " +(A+B+C));
+
+ 
+ }
